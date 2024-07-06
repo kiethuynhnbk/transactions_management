66878912d601a2efa70c9a0c3266357f0a4d06cb
unknown <kiethuynhnbk2000@gmail.com>
2024-07-06 16:49:40 +0700

    Init source

---
+++ src/main/java/com/example/transaction_management/RestController/PublicController.java
@@ -0,0 +1,16 @@
+package com.example.transaction_management.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
